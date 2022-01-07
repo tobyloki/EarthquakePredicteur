f42687d10339a95e39298f6678ed153434ef8fd4
tobyloki5 <tobyloki5@gmail.com>
2022-01-06 21:03:40 -0800

    init commit

---
+++ page.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.title('This is a title')
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)

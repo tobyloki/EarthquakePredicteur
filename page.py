511b1081c8e69b2bf2e32c30f9eed25e9aba7c5f
tobyloki <tobyloki5@gmail.com>
2022-01-09 11:33:55 -0800

    (1) page is finished + added images (2) compiled a subset of test data for showcasing accuracy of model

--- page.py
+++ page.py
@@ -26,60 +26,79 @@ st.image(image, caption='Train data (bottom section)')
 image = Image.open('images/middle.png')
 st.image(image, caption='Train data (middle section)')
 
-
 st.subheader('Feature engineering')
 
-# with st.container():
-#     col1, col2 = st.columns(2)
-#     with col1:
-#         st.markdown(f'### Time to next earthquake: `{5.61}s`')
-#     with col2:
-#         st.subheader('Magnitude:')
 with st.container():
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.info('Mean\n- **Description**: The average signal data value amongst the block\n- **Reason for selection**: The expected signal value at a point in time is a standard statistic to measure when attempting to discover features about data trends in relation to the time till an earthquake.')
+        st.info('##### Mean\n- **Description**: The average signal data value amongst the block\n- **Reason for selection**: The expected signal value at a point in time is a standard statistic to measure when attempting to discover features about data trends in relation to the time till an earthquake.')
     with col2:
-        st.info('STD\n- **Description**: The standard deviation of the signal data\n- **Reason for selection**: The spread of the data signals are also important in describing a signal\'s patterns and susceptibility to noise.')
+        st.info('##### STD\n- **Description**: The standard deviation of the signal data\n- **Reason for selection**: The spread of the data signals are also important in describing a signal\'s patterns and susceptibility to noise.')
     with col3:
-        st.info('Min\n- **Description**: The minimum value\n- **Reason for selection**: Acoustic data contain many forms of noise, but often the noise has a gaussian (normal) distribution, so extreme values such as min and max can still be significant features for training.')
+        st.info('##### Min\n- **Description**: The minimum value\n- **Reason for selection**: Acoustic data contain many forms of noise, but often the noise has a gaussian (normal) distribution, so extreme values such as min and max can still be significant features for training.')
 with st.container():
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.info('Max\n- **Description**: The maximum value \n- **Reason for selection**: Acoustic data contain many forms of noise, but often the noise has a gaussian (normal) distribution, so extreme values such as min and max can still be significant features for training.')
+        st.info('##### Max\n- **Description**: The maximum value \n- **Reason for selection**: Acoustic data contain many forms of noise, but often the noise has a gaussian (normal) distribution, so extreme values such as min and max can still be significant features for training.')
     with col2:
-        st.info('Kurtosis\n- **Description**: The tailedness (behavior of the data distribution along the extreme ends)\n- **Reason for selection**: This statistic help indicate the spread of the data of the ends in particular, which is not covered specifically with just the standard deviation.')
+        st.info('##### Peak count\n- **Description**: The counted number of local signal peaks that are above desired threshold of 100\n- **Reason for selection**: By counting the number of peaks, we can see if large number of peaks are indicators of trends in the data, rather than just noise and outliers.')
     with col3:
-        st.info('Skew\n- **Description**: The direction of asymmetry of the data’s distributions.\n- **Reason for selection**: Many of the previous statistics work extremely well when dealing with symmetrical and normally distributed data, but skew will help to describe scenarios where the signals are not truly symmetrical, which could be helpful in predicting.')
+        st.info('##### Quantile\n- **Description**: The data signal value where X% of all other signal values are smaller than it.\n- **Reason for selection**: By only using specific quantile ranges, we are able to identify trends in specific ranges of the bell curve and eliminate outliers.')
 with st.container():
-    col1, col2, col3 = st.columns(3)
+    col1, col2 = st.columns(2)
     with col1:
-        st.info('Quantile\n- **Description**: The data signal value where X% of all other signal values are smaller than it.\n- **Reason for selection**: ')
+        st.info('##### Kurtosis\n- **Description**: The tailedness (behavior of the data distribution along the extreme ends)\n- **Reason for selection**: This statistic help indicate the spread of the data of the ends in particular, which is not covered specifically with just the standard deviation.')
     with col2:
-        st.info('IQR\n- **Description**: The range between the 25th quantile and the 75th quantile\n- **Reason for selection**: ')
-    with col3:
-        st.info('STD with no peaks\n- **Description**: The standard deviation of data signals points that within 2 standard deviations of the sample mean.\n- **Reason for selection**: ')
+        image = Image.open('images/kurtosis.png')
+        st.image(image, caption='Kurtosis')
 with st.container():
-    col1, col2, col3 = st.columns(3)
+    col1, col2 = st.columns(2)
     with col1:
-        st.info('MFCC\n- **Description**: \n- **Reason for selection**: ')
+        st.info('##### Skew\n- **Description**: The direction of asymmetry of the data’s distributions.\n- **Reason for selection**: Many of the previous statistics work extremely well when dealing with symmetrical and normally distributed data, but skew will help to describe scenarios where the signals are not truly symmetrical, which could be helpful in predicting.')
     with col2:
-        st.info('Power spectrum density\n- **Description**: \n- **Reason for selection**: ')
-    with col3:
-        st.info('Peak count\n- **Description**: The counted number of local signal peaks that are above desired threshold of 100\n- **Reason for selection**: ')
+        image = Image.open('images/skew.png')
+        st.image(image, caption='Skew')
 with st.container():
-    col1, col2, col3 = st.columns(3)
+    col1, col2 = st.columns(2)
     with col1:
-        st.info('20th Quantile of Rolling standard deviation\n- **Description**: The 20th quantile of the distribution of standard deviations that are found through a rolling sampling of the data signals.\n- **Reason for selection**: ')
+        st.info('##### IQR\n- **Description**: The range between the 25th quantile and the 75th quantile\n- **Reason for selection**: By only looking solely at the interquartile range, we can looking at the middle most data to identify trends in the bulk of the data.')
     with col2:
-        st.info('80th Quantile of Rolling standard deviation\n- **Description**: The 80th quantile of the distribution of standard deviations that are found through a rolling sampling of the data signals.\n- **Reason for selection**: ')
+        st.info('##### STD with no peaks\n- **Description**: The standard deviation of data signals points that within 2 standard deviations of the sample mean.\n- **Reason for selection**: By eliminating the peaks before calculating standard deviation, we can eliminate outliers before calculating the standard deviation of the bell curve, allowing us to get a more accurate view of the trends of our data without excess noise.')
+with st.container():
+    col1, col2 = st.columns(2)
+    with col1:
+        st.info('##### MFCC\n- **Description**: MFCC is used for processing sound, and is a representation of its short run power spectrum.\n- **Reason for selection**: Because all of the data is based on sound frequencies, it is critical to capture the short run trends of these freqencies in the power spectrum, so that we can identify if they are representative of a trend towards the time of failure.')
+    with col2:
+        image = Image.open('images/mfcc.png')
+        st.image(image, caption='MFCC')
+with st.container():
+    col1, col2 = st.columns(2)
+    with col1:
+        st.info('##### Power spectrum density\n- **Description**: The power spectrum density measures the power of sound singal\'s power against a certain frequency.\n- **Reason for selection**: The power spectrum density is critical for capture trends in the power of the sound signal in the data against certain frequency, thus allowing us to identify if these trends are indicative of a trend towards the time of failure.')
+    with col2:
+        image = Image.open('images/powerSpectrumDensity.png')
+        st.image(image, caption='Power spectrum density')
+with st.container():
+    col1, col2 = st.columns(2)
+    with col1:
+        st.info('##### 20th Quantile of Rolling standard deviation\n- **Description**: The 20th quantile of the distribution of standard deviations that are found through a rolling sampling of the data signals.\n- **Reason for selection**: By rolling through sample portions of the data, it helps to identify trends in the short run within a specific window and in the long run between separate windows. In addition, using the 20th quantile we are able to looking exclusively at the lowest values in the data to identify trends in this specific subset of data. In addition, by using standard deviation, we can identify if this data is getting more concentrated or more sparce within the quantile, allowing us to better identify if we are getting closer to the time of failure.')
+    with col2:
+        st.info('##### 80th Quantile of Rolling standard deviation\n- **Description**: The 80th quantile of the distribution of standard deviations that are found through a rolling sampling of the data signals.\n- **Reason for selection**: Similarly to the 20th Quantile of Rolling standard deviation, by rolling through sample portions of the data, it helps to identify trends in the short run within a specific window and in the long run between separate windows. In addition, using the 20th quantile we are able to looking exclusively at the lowest values in the data to identify trends in this specific subset of data. In addition, by using standard deviation, we can identify if this data is getting more concentrated or more sparce within the quantile, allowing us to better identify if we are getting closer to the time of failure.')
+with st.container():
+    col1, col2 = st.columns(2)
+    with col1:
+        image = Image.open('images/rollingWindow.png')
+        st.image(image, caption='Rolling window')
+    with col2:
+        image = Image.open('images/rollingWindowStd.png')
+        st.image(image, caption='Rolling standard deviation')
 
 st.subheader('Feature selection')
-st.success('Catboost\'s `select_features`\n- Select the best features and drop harmful features from the dataset.\n- Removes complexity and increases interpretability')
+st.success('##### Catboost\'s `select_features`\n- Select the best features and drop harmful features from the dataset.\n- Removes complexity and increases interpretability\n- Catboost utilizes a function called select_features that allows it to take in a collection of input training data and possible features, and then returns back which features in the most reliable and should be selected for use in this model after a series of many iterations. In addition, it also makes an effort to avoid overfitting and monitors changes in metrics.')
 st.info('- https://catboost.ai/en/docs/concepts/python-reference_catboost_select_features')
 
 st.subheader('Model selection')
-st.success('Catboost\n- Gradient boosting algorithm')
+st.success('##### What is Gradient Boosting?\n- Method for both regression and classification problems\n- Produces prediction model as a collection of many weak prediction models (typically decision trees)\n- When new model added, all models reweighted so stronger models are weighted higher and weaker models are weighted lower\n- Thus iteratively improves upon many weak prediction models to create one strong aggregate model that is better than the sum of its parts.\n\n##### What is CatBoost?\n- Version of Gradient Boosting, tends to work very well compared to similar models\n- Automatically converts categorical values into numbers\n- Then able to combine categorical and numerical features\n- Combines many predictors together, giving weights to each, for a single aggregate model')
 
 st.subheader('Model metrics')
 
@@ -150,9 +169,4 @@ if uploaded_file is not None:
     # Making prediction 
     preds = model.predict(X_test)[0]
 
-with st.container():
-    col1, col2 = st.columns(2)
-    with col1:
-        st.markdown(f'##### Time til\' next earthquake: `{preds:.2f}s`')
-    # with col2:
-    #     st.markdown(f'##### Confidence: `{int(0.86*100)}%`')
+st.markdown(f'##### Time til\' next earthquake: `{preds:.2f}s`')
